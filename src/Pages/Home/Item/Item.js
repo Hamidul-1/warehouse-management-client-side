72356619b8e679ad17eab634531de9fbfc3927c5
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-01 03:16:41 +0600

    items added

---
+++ src/Pages/Home/Item/Item.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Item.css';
+
+const Item = ({item}) => {
+    const {name, img, description, price, quantity, supplier} = item;
+    return (
+        <div className='item'>
+            <img src={img} alt="" />
+            <h2>Service: {name}</h2>
+            <p>Price: {price}</p>
+            <p><small>{description}</small></p>
+            <p><small>{quantity}</small></p>
+            <p><small>{supplier}</small></p>
+            <button>Stock Update {name}</button>
+        </div>
+    );
+};
+
+export default Item;
--- src/Pages/Home/Items/Items.js
+++ src/Pages/Home/Items/Items.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Item from '../Item/Item';
+import './Items.css'
 
 const Items = () => {
+
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch('items.json')
+            .then(res => res.json())
+            .then(data => setItems(data))
+
+    }, [])
+
     return (
         <div>
-            <h2>Inventory Items</h2>
+            <h2 className='items-title'>Inventory Items</h2>
+            <div className="items-container">
+                {
+                    items.map(item => <Item
+                        key={item.id}
+                        item={item}
+                    >
+                    </Item>)
+                }
+            </div>
         </div>
     );
 };
