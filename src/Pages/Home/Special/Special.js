fe620a6bf3f0762d4dcdd2878fae0992571f0836
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-01 15:04:35 +0600

    specials added

--- src/Pages/Home/Special/Special.js
+++ src/Pages/Home/Special/Special.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Special = () => {
+const Special = ({ special }) => {
+    const { name, img } = special;
+
     return (
-        <div>
-            <h2>Special</h2>
+        <div className='g-5 col-sm-12 col-md-6 col-lg-4'>
+            <div class="card " style={{ width: "18rem" }}>
+                <img src={img} class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">{name}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Specials/Specials.js
+++ src/Pages/Home/Specials/Specials.js
@@ -3,6 +3,7 @@ import React from 'react';
 import special1 from '../../../images/macbook-pro.jpg'
 import special2 from '../../../images/M2_Apple_MacBook_Pro_14_Mac_Air_2022_drdNBC.jpg'
 import special3 from '../../../images/mac-air.jpg'
+import Special from '../Special/Special';
 
 const specials = [
     {id: 1, name: 'Mackbook Pro', img: special1},
@@ -12,8 +13,16 @@ const specials = [
 
 const Specials = () => {
     return (
-        <div>
+        <div className='container'>
             <h2 className='text-primary text-center'>Special Arrival</h2>
+            <div className="raw">
+                {
+                    specials.map(special => <Special
+                        key={special.id}
+                        special={special}
+                    ></Special>)
+                }
+            </div>
         </div>
     );
 };
