41f81c919e5cd30bf8f2a0961820c2cf1a54179c
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-06 23:45:27 +0600

    added checkout

---
+++ src/Pages/Shared/Checkout/Checkout/Checkout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Checkout = () => {
+    return (
+        <div>
+            Please checkout your booking
+        </div>
+    );
+};
+
+export default Checkout;
